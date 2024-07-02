e4794861acae19a03e029bcf80c4162feb3453d5
jpphang <joyanna_phang1@my.vcccd.edu>
2024-07-02 15:35:57 -0700

    44

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 def main():
 
     numbers = []
+    
+    n1 = int(input("Enter first number:"))
+    n2 = int(input("Enter 2nd number:"))
+    n3 = int(input("Enter 3rd number:"))
+    n4 = int(input("Enter 4th number:"))
+    n5 = int(input("Enter 5th number:"))
     """
     ########################################
     Code Your Program here
